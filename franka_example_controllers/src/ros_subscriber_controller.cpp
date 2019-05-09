b0e195a40df63baa8db9ff93d33de59a451b4709
fhainzl <f.hainzl@outlook.com>
2019-05-09 17:25:37 +0200

    Update position controller

--- franka_example_controllers/src/ros_subscriber_controller.cpp
+++ franka_example_controllers/src/ros_subscriber_controller.cpp
@@ -12,115 +12,122 @@
 
 namespace franka_example_controllers {
 
-bool RosSubscriberController::init(hardware_interface::RobotHW* robot_hardware,
-                                          ros::NodeHandle& node_handle) {
+    bool RosSubscriberController::init(hardware_interface::RobotHW* robot_hardware,
+                                       ros::NodeHandle& node_handle) {
 
-  velocity_joint_interface_ = robot_hardware->get<hardware_interface::VelocityJointInterface>();
+        velocity_joint_interface_ = robot_hardware->get<hardware_interface::VelocityJointInterface>();
+        if (velocity_joint_interface_ == nullptr) {
+          ROS_ERROR(
+              "RosSubscriberController: Error getting velocity joint interface from hardware!");
+          return false;
+        }
 
-  if (velocity_joint_interface_ == nullptr) {
-    ROS_ERROR(
-        "RosSubscriberController: Error getting velocity joint interface from hardware!");
-    return false;
-  }
+        std::vector<std::string> joint_names;
+        if (!node_handle.getParam("joint_names", joint_names)) {
+            ROS_ERROR("RosSubscriberController: Could not parse joint names");
+        }
 
-  position_joint_interface_ = robot_hardware->get<hardware_interface::PositionJointInterface>();
-  if (position_joint_interface_ == nullptr) {
-    ROS_ERROR(
-            "JointPositionExampleController: Error getting position joint interface from hardware!");
-    return false;
-  }
+        if (joint_names.size() != 7) {
+            ROS_ERROR_STREAM("RosSubscriberController: Wrong number of joint names, got "
+                                     << joint_names.size() << " instead of 7 names!");
+            return false;
+        }
 
-  std::vector<std::string> joint_names;
-  if (!node_handle.getParam("joint_names", joint_names)) {
-    ROS_ERROR("RosSubscriberController: Could not parse joint names");
-  }
+        velocity_joint_handles_.resize(7);
+        for (size_t i = 0; i < 7; ++i) {
+          try {
+            velocity_joint_handles_[i] = velocity_joint_interface_->getHandle(joint_names[i]);
+          } catch (const hardware_interface::HardwareInterfaceException &ex) {
+            ROS_ERROR_STREAM(
+                    "RosSubscriberController: Exception getting joint handles: " << ex.what());
+            return false;
+          }
+        }
 
-  if (joint_names.size() != 7) {
-    ROS_ERROR_STREAM("RosSubscriberController: Wrong number of joint names, got "
-                     << joint_names.size() << " instead of 7 names!");
-    return false;
-  }
+        joint_command_.velocity.resize(7);
+        joint_command_.position.resize(7);
+        for (size_t i = 0; i < 7; ++i) {
+            joint_command_.velocity.at(i) = 0.0f;
+            joint_command_.position.at(i) = 0.0f;
+        }
 
-  velocity_joint_handles_.resize(7);
-  for (size_t i = 0; i < 7; ++i) {
-    try {
-      velocity_joint_handles_[i] = velocity_joint_interface_->getHandle(joint_names[i]);
-    } catch (const hardware_interface::HardwareInterfaceException &ex) {
-      ROS_ERROR_STREAM(
-              "RosSubscriberController: Exception getting joint handles: " << ex.what());
-      return false;
-    }
-  }
+        joint_command_subscriber_ = node_handle.subscribe ("controller_command/joint_command",
+                                                           10, &RosSubscriberController::joint_state_callback, this);
 
-    position_joint_handles_.resize(7);
-    for (size_t i = 0; i < 7; ++i) {
-      try {
-        position_joint_handles_[i] = position_joint_interface_->getHandle(joint_names[i]);
-      } catch (const hardware_interface::HardwareInterfaceException &e) {
-        ROS_ERROR_STREAM(
-                "JointPositionExampleController: Exception getting joint handles: " << e.what());
-        return false;
-      }
+        return true;
     }
 
-      joint_command_.velocity.resize(7);
-      for (size_t i = 0; i < 7; ++i) {
-        joint_command_.velocity.at(i) = 0.0f;
-        joint_command_.position.at(i) = position_joint_handles_[i].getPosition();
-      }
-
-      joint_command_subscriber_ = node_handle.subscribe ("controller_command/joint_command",
-            10, &RosSubscriberController::joint_state_callback, this);
-
-  return true;
-}
-
-void RosSubscriberController::starting(const ros::Time& /* time */) {
-}
-
-void RosSubscriberController::update(const ros::Time& /* time */,
-                                            const ros::Duration& period) {
-  bool set_position = false;
-  for (size_t i = 0; i < 7; ++i) {
-    if (joint_command_.position.at(i) != 0.0f) {
-        set_position = true;
-        break;
+    void RosSubscriberController::starting(const ros::Time& /* time */) {
     }
-  }
 
-  if (set_position) {
-    if (joint_command_.position.size() != 7) {
-      ROS_ERROR_STREAM(
-              "RosSubscriberController: Was expecting 7 values for the joint position. ");
-    } else {
-      for (size_t i = 0; i < 7; ++i) {
-        position_joint_handles_.at(i).setCommand(joint_command_.position.at(i));
-      }
+    void RosSubscriberController::update(const ros::Time& /* time */,
+                                         const ros::Duration& period) {
+        bool set_position = false;
+        if (joint_command_.position.size() == 7) {
+            for (size_t i = 0; i < 7; ++i) {
+                if (joint_command_.position.at(i) != 0.0f) {
+                    set_position = true;
+                    break;
+                }
+            }
+        } else if (joint_command_.position.size() == 0) {
+            set_position = false;
+        } else {
+            ROS_ERROR_STREAM(
+                    "RosSubscriberController: Was expecting 7 or 0 values for the joint position. ");
+        }
+
+        if (set_position) {
+            for (size_t i = 0; i < 7; ++i) {
+                float current_position = velocity_joint_handles_.at(i).getPosition();
+                float target_position = joint_command_.position.at(i);
+                float position_deviation = target_position - current_position;
+                float epsilon = 0.004; //The deviation which is allowed from a position
+                float scaling = 1.2; //scaling factor for the controller
+                float controlled_velocity = scaling * position_deviation;
+                float max_velocity = 1.0;
+                float min_velocity = 0.1;
+                bool velocity_is_negative = false;
+
+                if (position_deviation < 0) {
+                    position_deviation *= -1;
+                    velocity_is_negative = true;
+                }
+
+                if (position_deviation > epsilon) {
+                    if (controlled_velocity > max_velocity) {
+                        controlled_velocity = max_velocity;
+                    }
+                    if (controlled_velocity < min_velocity) {
+                        controlled_velocity = min_velocity;
+                    }
+
+                    if () {
+                        position_deviation *= -1;
+                        velocity_is_negative = false;
+                    }
+
+                    velocity_joint_handles_.at(i).setCommand(controlled_velocity);
+              }
+            }
+        } else {
+          if (joint_command_.velocity.size() != 7) {
+            ROS_ERROR_STREAM(
+                    "RosSubscriberController: Was expecting 7 values for the joint velocity. ");
+          } else {
+            for (size_t i = 0; i < 7; ++i) {
+              velocity_joint_handles_.at(i).setCommand(joint_command_.velocity.at(i));
+            }
+          }
+        }
+
     }
-  } else {
-    if (joint_command_.velocity.size() != 7) {
-      ROS_ERROR_STREAM(
-              "RosSubscriberController: Was expecting 7 values for the joint velocity. ");
-    } else {
-      for (size_t i = 0; i < 7; ++i) {
-        velocity_joint_handles_.at(i).setCommand(joint_command_.velocity.at(i));
-      }
+
+    void RosSubscriberController::joint_state_callback (const sensor_msgs::JointState joint_command) {
+        joint_command_ = joint_command;
     }
-  }
-
-}
-
-void RosSubscriberController::stopping(const ros::Time& /*time*/) {
-  // WARNING: DO NOT SEND ZERO VELOCITIES HERE AS IN CASE OF ABORTING DURING MOTION
-  // A JUMP TO ZERO WILL BE COMMANDED PUTTING HIGH LOADS ON THE ROBOT. LET THE DEFAULT
-  // BUILT-IN STOPPING BEHAVIOR SLOW DOWN THE ROBOT.
-}
-
-void RosSubscriberController::joint_state_callback (const sensor_msgs::JointState joint_command) {
-  joint_command_ = joint_command;
-}
 
 }  // namespace franka_example_controllers
 
 PLUGINLIB_EXPORT_CLASS(franka_example_controllers::RosSubscriberController,
-                       controller_interface::ControllerBase)
+        controller_interface::ControllerBase)
